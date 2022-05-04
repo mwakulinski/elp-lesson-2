af98f9a225a73e81dde2f7d4c3db316dd8b31e99
mwakulinski <michal.wakulinski1@gmail.com>
2022-05-04 15:01:46 +0200

    implements setTimeoutInMinutes test to check that callback is called at a specific time

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,14 @@
+import { doesNotMatch } from "assert";
+
 export class TimeoutSetter {
   setTimeoutInSeconds(callback: () => void, seconds: number) {
     setTimeout(() => {
       callback();
     }, seconds * 1000);
+  }
+
+  setTimeoutInMinutes(callback: () => void, minutes: number) {
+    this.setTimeoutInSeconds(callback, minutes * 60);
   }
 }
 describe("test TimeoutSetter with ordinary callbacks", () => {
@@ -43,5 +49,37 @@ describe("test TimeoutSetter with ordinary callbacks", () => {
     expect(mockFunction).toHaveBeenCalledTimes(1);
 
     jest.useRealTimers();
+  });
+
+  describe("test setTimeoutInMinutes", () => {
+    it("should call callback", (done) => {
+      const mockFunction = jest.fn();
+      expect.assertions(1);
+
+      const callback = () => {
+        mockFunction();
+        expect(mockFunction).toHaveBeenCalled();
+        done();
+      };
+
+      new TimeoutSetter().setTimeoutInMinutes(callback, 0.01);
+    });
+
+    it("should call callback at a specific time", (done) => {
+      jest.useFakeTimers();
+      const mockFunction = jest.fn();
+
+      const callback = () => {
+        mockFunction();
+        done();
+      };
+
+      new TimeoutSetter().setTimeoutInMinutes(callback, 1);
+      jest.advanceTimersByTime(60 * 1000 - 1);
+      expect(mockFunction).not.toBeCalled();
+
+      jest.advanceTimersByTime(1);
+      expect(mockFunction).toBeCalledTimes(1);
+    });
   });
 });
